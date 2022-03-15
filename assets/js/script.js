29e203cf29cd403e0a46275fcb54019d2f34d853
Villager <deiondrae@gmail.com>
2022-03-15 18:30:53 -0400

    new task functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,25 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
 
-    var listItemEl = document.createElement("li")
+    //create list item
+    var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //make div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    // add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl)
+    
+    //add list item to list
     tasksToDoEl.appendChild(listItemEl);
-    console.log(event);
 };
 
 formEl.addEventListener("submit", createTaskHandler);
